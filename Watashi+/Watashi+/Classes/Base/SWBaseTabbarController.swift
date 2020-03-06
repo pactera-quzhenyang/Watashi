cb0a95fdee0802653f5b15ab8b0aa288ba304285
pactera-quzhenyang <zhenyang_yan12@163.com>
2020-03-06 11:16:02 +0800

    2

--- Watashi+/Watashi+/Classes/Base/SWBaseTabbarController.swift
+++ Watashi+/Watashi+/Classes/Base/SWBaseTabbarController.swift
@@ -6,12 +6,49 @@
 //  Copyright © 2020 曲振阳. All rights reserved.
 //
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class SWBaseTabbarController: UITabBarController {
+
+    lazy var lineView: UIView = {
+        let lineView = UIView()
+        lineView.backgroundColor = baseColor
+        return lineView
+    }()
+
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        for (index, item) in tabBar.items!.enumerated() {
+            if index == 2 || index == 3 {
+                item.badgeValue = "6"
+                item.badgeColor = baseColor
+            }
+        }
+        tabBar.addSubview(lineView)
+        lineView.snp.makeConstraints { (make) in
+            make.top.equalTo(0)
+            make.left.equalTo(0)
+            make.width.equalTo(screenWidth / 5)
+            make.height.equalTo(1)
+        }
+
+        self.rx.didSelect.subscribe(onNext: { (selectController) in
+            for (index, item) in self.viewControllers!.enumerated() {
+                if selectController == item {
+                    UIView.animate(withDuration: 0.3) {
+                        self.lineView.snp.updateConstraints { (make) in
+                            make.left.equalTo(Int(screenWidth / 5) * index)
+                        }
+                        self.view.layoutIfNeeded()
+                    }
+                }
+            }
+            }).disposed(by: disposeBag)
     }
 }
